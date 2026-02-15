33407dc9f4c0b5884743f746b3219c43e4ccf70c
Ashraf Shahin <ashraf.hossein.web@gmail.com>
2026-02-15 07:42:17 +0000

    database created on mongoDB, collections created, mongoose and mongoDB installed, npm install mongodb mongoose, by that command, Connected to the Project by mongoose.connect and 3 things to change on the link, 1. userName, Password and clusterName... please see the codes now...

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
 
 app.use(express.json())
 
+mongoose.connect('mongodb+srv://shahin_db_user:BaoxjkvRUjnlT6DW@clustershahin.nicn5ni.mongodb.net/?appName=ClusterShahin').then(() => {
+    console.log('Database Connected');
+
+})
+
 app.post('/reg', (req, res) => {
     console.log(req.body);
-    
+
 })
 
 app.listen(5000, () => {
     console.log('server running');
-    
+
 })
+
